540a519bfee701b2a277a5483c242a262f6dcf17
lideracademicoaifi-sketch <lideracademicoaifi@gmail.com>
2026-04-23 15:48:41 -0500

    Update ExamenClient.js

--- app/examen/ExamenClient.js
+++ app/examen/ExamenClient.js
@@ -3,6 +3,11 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { supabase } from '../lib/supabase'
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { supabase } from '../lib/supabase'
 
 export default function ExamenClient() {
   const searchParams = useSearchParams()
